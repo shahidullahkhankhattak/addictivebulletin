7dc384997b022edb22814b2ed315b46a2768628e
Karma <shahid.khan@mobilelive.ca>
2019-12-20 13:20:31 +0500

    added source logo

--- resources/views/inc/home/content.blade.php
+++ resources/views/inc/home/content.blade.php
@@ -4,11 +4,11 @@
             @include('inc.sidebar.left-main')
         </div>
         <div class="article desc" v-if="source" style="padding-top:100px;padding-top: 100px;box-shadow: 0px 0px 10px lightgrey;margin-bottom:-70px;display:none" v-show="source">
-                <div class="loader slideDown" @click="$router.push('/'+lang)">
-                    <h3 v-html="sources.filter(s => s.slug == source)[0].source"></h3>
-                    <span v-if="lang == 'english'" v-html="'view all sources'"></span>
-                    <span v-else v-html="'سب زرائے سے خبریں دکھا یں'"></span>
-                </div>
+            <div class="loader slideDown" @click="$router.push('/'+lang)">
+                <h3 v-html="sources.filter(s => s.slug == source)[0].source"></h3>
+                <span v-if="lang == 'english'" v-html="'view all sources'"></span>
+                <span v-else v-html="'سب زرائے سے خبریں دکھا یں'"></span>
+            </div>
         </div>
         <div class="article" v-if="directLoad" style="margin-bottom:30px; !important;display:none">
             <div class="ui link auto cards">
@@ -18,17 +18,18 @@
                 @foreach($news as $article)
                 <article class="card">
                     <div class="image">
-                        <router-link
-                            to="/{{$lang == "en" ? 'english' : 'urdu' }}/news/{{$article->id}}/{{$article->slug ? $article->slug : ''}}">
-                            <img onload="fadeIn(this)" alt="{{$article->title}}"
-                                src="/thumb.php?w=200&h=150&src={{$article->media}}" onerror="this.src = '/public/images/no_image.jpg'">
+                        <router-link to="/{{$lang == 'en' ? 'english' : 'urdu' }}/news/{{$article->id}}/{{$article->slug ? $article->slug : ''}}">
+                            <router-link to="/{{$lang == 'en' ? 'english' : 'urdu' }}/source/{{$article->source_slug}}" class="ui image label source-label" style="background-color: {{$article->color}} !important" title="{{$article->source}}">
+                                <img src="/public/images/source_logo/{{$article->source_slug}}.png" alt="{{$article->source}}">
+                                {{$article->source}}
+                            </router-link>
+                            <img onload="fadeIn(this)" alt="{{$article->title}}" src="/thumb.php?w=200&h=150&src={{$article->media}}" onerror="this.src = '/public/images/no_image.jpg'">
                         </router-link>
                     </div>
                     <div class="content">
                         <header class="header">
                             <a class="no-show" title="{{$article->title}}" href="/{{$lang == 'en' ? 'english' : 'urdu' }}/news/{{$article->id}}/{{$article->slug ? $article->slug : ''}}"></a>
-                            <router-link
-                                to="/{{$lang == "en" ? 'english' : 'urdu' }}/news/{{$article->id}}/{{$article->slug ? $article->slug : ''}}">
+                            <router-link to="/{{$lang == "en" ? 'english' : 'urdu' }}/news/{{$article->id}}/{{$article->slug ? $article->slug : ''}}">
                                 {{$article->title}}</router-link>
                         </header>
                         <div class="meta">
@@ -55,16 +56,17 @@
                 <div class="no-news" v-else-if="lang == 'urdu' && stories.length == 0 && !loading">منتخب فلٹر میں کوی خبر نہی ملی.</div>
                 <article class="card" v-for="story in stories">
                     <div class="image">
-                        <router-link
-                            :to="'/'+lang+'/news/'+story.id+'/'+(story.slug ? story.slug : '')">
-                            <img onload="fadeIn(this)"
-                                :src="'/thumb.php?w=200&h=150&src=' + story.media" onerror="this.src = '/public/images/no_image.jpg'">
+                        <router-link :to="'/'+lang+'/news/'+story.id+'/'+(story.slug ? story.slug : '')">
+                            <router-link :to="`/${lang}/source/${story.source_slug}`" class="ui image label source-label" :style="{'background-color': `${story.color} !important`}" :title="story.source">
+                                <img :src="`/public/images/source_logo/${story.source_slug}.png`" :alt="story.source">
+                                @{{story.source}}
+                            </router-link>
+                            <img onload="fadeIn(this)" :src="'/thumb.php?w=200&h=150&src=' + story.media" onerror="this.src = '/public/images/no_image.jpg'">
                         </router-link>
                     </div>
                     <div class="content">
                         <header class="header">
-                            <router-link
-                                :to="'/'+lang+'/news/'+story.id+'/'+(story.slug ? story.slug : '')">
+                            <router-link :to="'/'+lang+'/news/'+story.id+'/'+(story.slug ? story.slug : '')">
                                 @{{story.title}}</router-link>
                         </header>
                         <div class="meta">
@@ -83,9 +85,9 @@
             </div>
         </div>
         <div class="article slideDown" v-if="loading" :style="{'padding': '20px', 'margin-top': '-60px', 'margin-bottom': '0px'}">
-                <div class="loader">
-                        <img src="/public/images/loader.gif" alt="">
-                </div>
+            <div class="loader">
+                <img src="/public/images/loader.gif" alt="">
+            </div>
         </div>
     </div>
 </div>
